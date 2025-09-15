bbd365c0231372bf52a944fbdd4366771f352ca5
boginskiy <1.boginskiy@mail.ru>
2025-09-15 09:57:45 +0300

    add table 'urls'

--- internal/db/storeDB.go
+++ internal/db/storeDB.go
@@ -5,6 +5,7 @@ import (
 
 	c "github.com/boginskiy/Clicki/cmd/config"
 	l "github.com/boginskiy/Clicki/internal/logger"
+	_ "github.com/lib/pq"
 )
 
 type StoreDB struct {
@@ -17,6 +18,17 @@ func NewStoreDB(kwargs c.VarGetter, logger l.Logger) (*StoreDB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// "Самостоятельное" создание таблиц"
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS urls (
+			id SERIAL PRIMARY KEY,
+			original_url TEXT NOT NULL,
+			short_url VARCHAR(8) NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`)
+	if err != nil {
+		return nil, err
+	}
+
 	return &StoreDB{
 		Logger: logger,
 		DB:     db,
