aa948c2a98a0c6a1f803faa31dc980b49e511e75
boginskiy <1.boginskiy@mail.ru>
2025-12-09 00:48:42 +0300

    add global var

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,10 +4,19 @@ import (
 	"github.com/boginskiy/Clicki/cmd/config"
 	"github.com/boginskiy/Clicki/internal/app"
 	"github.com/boginskiy/Clicki/internal/logg"
+	"github.com/boginskiy/Clicki/pkg"
+)
+
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
 )
 
 // main - start of Appl.
 func main() {
+	pkg.PrintInfo(buildVersion, buildDate, buildCommit)
+
 	logger := logg.NewLogg("main.log", "FATAL")
 	cfg := config.NewVariables(logger)
 
