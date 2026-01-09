6bf6d744fbddfa285dead184a780159b7d623e25
boginskiy <1.boginskiy@mail.ru>
2026-01-09 13:23:24 +0300

    add tests for gRPC serv

--- internal/handler/shortenerHdr_test.go
+++ internal/handler/shortenerHdr_test.go
@@ -21,9 +21,46 @@ func TestShortenerService(t *testing.T) {
 	srv := InitShortenerService(pathToLogg)
 
 	testShortenURL(t, srv)
+	testExpandURL(t, srv)
 
 	defer tfunc.DeleteTestFiles(pathToLogg)
 
+}
+
+func testExpandURL(t *testing.T, srv *handler.ShortenerService) {
+	tests := []struct {
+		name    string
+		bodyReq string
+		codeRes int
+		dataRes int
+	}{
+		{"test negative ExpandURL gRPC request", "XXXXXXXX", 3, 0},
+		{"test positive ExpandURL gRPC request", "wrs4db6j", 0, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Context
+			ctx := metadata.AppendToOutgoingContext(context.Background(), "authorization", "XXX")
+			ctx = context.WithValue(context.Background(), auth.CtxUserID, 100)
+
+			res, err := srv.ExpandURL(ctx, &rpc.URLExpandRequest{Id: tt.bodyReq})
+
+			// Check error.
+			grpcStatus, ok := status.FromError(err)
+			if ok {
+
+				if int(grpcStatus.Code()) != tt.codeRes {
+					t.Errorf("%s:\n\texpected: %v\n\tactual: %v", tt.name, tt.codeRes, int(grpcStatus.Code()))
+				}
+
+			} else {
+				// Check response.
+				if len(res.Result) == tt.dataRes {
+					t.Errorf("%s:\n\texpected: >%v\n\tactual: %v", tt.name, tt.dataRes, len(res.Result))
+				}
+			}
+		})
+	}
 }
 
 func testShortenURL(t *testing.T, srv *handler.ShortenerService) {
